786c0ea5ff88e49f2d638b0ae6dc86b19c320e4a
RyanTyler <RyanTyler@hvs-rtyler22-mbpr.wusm-wifi.wucon.wustl.edu>
2022-10-13 15:01:12 -0400

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,21 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenRadius(.01);
+		StdDraw.setPenColor((int)(Math.random()*256),(int)(Math.random()*256) , (int)(Math.random()*256));
+		StdDraw.filledRectangle(.5, .5, .4, .2);
+		StdDraw.setPenColor((int)(Math.random()*256),(int)(Math.random()*256) , (int)(Math.random()*256));
+		StdDraw.arc(.5, .3, .4, 0, 180);
+		StdDraw.arc(.5, .7, .4, 180, 360);
+		StdDraw.setPenColor((int)(Math.random()*256),(int)(Math.random()*256) , (int)(Math.random()*256));
+		double[] xCord = {.6,0.35,.65,.4,.5};
+		double[] yCord = {.35,.55,.55,.35,.65};
+		StdDraw.filledPolygon(xCord,yCord);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(.5, .5, .4, .2);
+		
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -15,10 +15,85 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+	
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled =  in.nextBoolean();
+		double parameterOne;
+		double parameterTwo;
+		double parameterThree;
+		double parameterFour;
+		double parameterFive;
+		double parameterSix;
+		
+		if(shapeType.equals("ellipse")) {
+			parameterOne = in.nextDouble();
+			parameterTwo = in.nextDouble();
+			parameterThree = in.nextDouble();
+			parameterFour = in.nextDouble();
+	
+			if(isFilled) {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		
+			
+		}
+		
+		if(shapeType.equals("rectangle")) {
+			parameterOne = in.nextDouble();
+			parameterTwo = in.nextDouble();
+			parameterThree = in.nextDouble();
+			parameterFour = in.nextDouble();
+			
+			if(isFilled) {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			
+			
+		}
+		
+		if(shapeType.equals("triangle")) {
+			parameterOne = in.nextDouble();
+			parameterTwo = in.nextDouble();
+			parameterThree = in.nextDouble();
+			parameterFour = in.nextDouble();
+			parameterFive = in.nextDouble();
+			parameterSix = in.nextDouble();
+			
+			double[] xCord = {parameterOne, parameterThree, parameterFive};
+			double[] yCord = {parameterTwo, parameterFour, parameterSix};
+		
+			if(isFilled) {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.filledPolygon(xCord,yCord);
+			}
+			else {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.polygon(xCord, yCord);
+			}
+			
+			
+		}
+		
+
+		
 		
 	}
 }
